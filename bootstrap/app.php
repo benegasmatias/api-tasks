0c8419238c4dd18f144348db49908567c335ca91
matias benegas <benegas.matias21@gmail.com>
2021-08-07 16:12:43 +0200

    eliminacion mailer

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -106,7 +106,6 @@ $app->register(App\Providers\AppServiceProvider::class);
 $app->register(App\Providers\AuthServiceProvider::class);
 $app->register(App\Providers\EventServiceProvider::class);
 $app->register(Tymon\JWTAuth\Providers\LumenServiceProvider::class);
-$app->register(Illuminate\Mail\MailServiceProvider::class);
 /*
 |--------------------------------------------------------------------------
 | Load The Application Routes
